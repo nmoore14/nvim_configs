cf615647df19eb7a588d17afa4c20f99f31ea0e5
Nick Moore <nickmoore_1@live.com>
2022-08-21 22:36:31 -0400

    Bandaid the telescope-fzf-native error for now

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -168,6 +168,6 @@ telescope.setup {
 }
 
 require("telescope").load_extension("ui-select")
-require("telescope").load_extension("fzf")
+-- require("telescope").load_extension("fzf")
 require('telescope').load_extension('dap')
 
--- lua/plugin.lua
+++ lua/plugin.lua
@@ -19,7 +19,7 @@ require('packer').startup(function(use)
 
   -- UI to select things (files, grep results, open buffers...)
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
   use { 'nvim-telescope/telescope-ui-select.nvim' }
 
   ---------------------------------------
